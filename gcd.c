9a9c3de3179ed51523fc2b24e1a56b4eea4cfa4a
scardona14 <cardanasean123@gmail.com>
2024-01-14 23:39:14 -0400

    gcd of two non-negative integer

--- gcd.c
+++ gcd.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// finds the greatest common divisor of two non negative integer values and return the result
+
 int gcd (int u, int v);
 
 int main (void)
 {
-    int result = 0;
-    result = gcd(150, 35);
-    printf("The gcd of 150 and 35 is %d\n", result);
+	int result = 0;
+	result = gcd(150, 35);
+	printf("The gcd of 150 and 35 is %d\n", result);
 
-    result = gcd(1026, 405);
-    printf("The gcd of 1026 and 405 is %d\n", result);
+	result = gcd(1026, 405);
+	printf("The gcd of 1026 and 405 is %d\n", result);
 
-    printf("The gcd of 83 and 240 is %d\n", gcd(83, 240));
-    
+	printf("The gcd of 83 and 240 is %d\n", gcd(83, 240));
 
-    return 0;
+
+	return 0;
 }
 
 int gcd (int u, int v)
 {
-    int temp;
+	int temp;
 
-    while (v != 0) {
-        temp = u % v;
-        u = v;
-        v = temp;
-    }
+	while (v != 0) {
+		temp = u % v;
+		u = v;
+		v = temp;
+	}
 
-    return u;
+	return u;
 }
 
